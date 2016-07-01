44f418b02e6c6c88091942650bcc56575ff5f546
Dan November <november@g.harvard.edu>
2016-07-01 14:45:51 -0400

    First commit for assignment 2!

---
+++ Assignment2/Assignment2/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  MainController.swift
+//  Assignment2
+//
+//  Created by Dan November on 7/1/16.
+//  Copyright © 2016 Dan November. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Assignment2/Assignment2/Problem4ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Problem4ViewController.swift
+//  Assignment2
+//
+//  Created by Dan November on 7/1/16.
+//  Copyright © 2016 Dan November. All rights reserved.
+//
+
+import UIKit
+
+class Problem4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Problem 4"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
